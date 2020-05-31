b1d25788891bebe2b8835622289cdc5c433df9e4
unknown <wkevg12@gmail.com>
2020-05-31 12:31:39 -0400

    Updates

---
+++ precache-manifest.b910851ea275ce02b387f4aed85d7c6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1377954af49ff1e57a8d9a782ebdd280",
+    "url": "/toDoList/index.html"
+  },
+  {
+    "revision": "83352988d33e3dee81c5",
+    "url": "/toDoList/static/css/main.f14f1282.chunk.css"
+  },
+  {
+    "revision": "52df4cb81203d577e615",
+    "url": "/toDoList/static/js/2.acd4805b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/toDoList/static/js/2.acd4805b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83352988d33e3dee81c5",
+    "url": "/toDoList/static/js/main.f391de53.chunk.js"
+  },
+  {
+    "revision": "97d67b6f9d823f38615d",
+    "url": "/toDoList/static/js/runtime-main.d8d114a3.js"
+  }
+]);
